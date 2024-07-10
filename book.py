7f0314da434aef3f618af6df74b2e5e7ba6574a9
Bhamare-Vinit <vinitbhamare2002@gmail.com>
2024-07-10 17:47:41 +0530

    [vinit] add:updated code to edit the entry in address book

--- book.py
+++ book.py
@@ -23,10 +23,34 @@ class Address_Book:
             print("No contacts found.")
 
 
+    def edit_entry(self,fname=None,lname=None,address=None,city=None,state=None,zip=None,phone_number=None,email=None):
+        if phone_number != None and phone_number in self.contact:
+            lst_info = self.contact[phone_number]
+            print(lst_info)
+            if fname != None:
+                lst_info["firstname"] = fname
+            if lname != None:
+                lst_info["lastname"] = lname
+            if address != None:
+                lst_info["address"] = address
+            if city != None:
+                lst_info["city"] = city
+            if state != None:
+                lst_info["state"] = state
+            if zip != None:
+                lst_info["zip"] = zip
+            if email != None:
+                lst_info["email"] = email
+            self.contact[phone_number] = lst_info
+            print("Data updated successfully")
+        else:
+            print("Phone number does not exists in the database")
+
+
     def console(self):
         while True:
             try:
-                print("\n1. Add Contact\n2. Delete Contact\n3. Edit Contact\n4. Search Contact\n5. View Contacts\n6. Stop")
+                print("\n1. Add Contact\n2. Display Book\n3. Edit Contact\n4. Search Contact\n5. View Contacts\n6. Stop")
                 n = int(input("Enter your options: "))
                 if n == 1:
                     fname = input("Enter first name: ")
@@ -54,6 +78,29 @@ class Address_Book:
 
                 if n==2:
                     self.display()
+                
+                if n==3:
+                    fname = input("Enter first name: ")
+                    lname= input("Enter last name: ")
+                    address = input("Address: ")
+                    city=input("Enter the city: ")
+                    state=input("Enter the state: ")
+                    zip=input("Enter the zip code: ")
+                    phone_number = input("Phone Number: ")
+                    email = input("Email: ")
+                    if len(fname) == 0:
+                        fname = None
+                    if len(lname) == 0:
+                        lname = None
+                    if len(address) == 0:
+                        address = None
+                    if len(city) == 0:
+                        city = None
+                    if len(phone_number) == 0:
+                        phone_number = None
+                    if len(email) == 0:
+                        email = None
+                    self.edit_entry(fname, lname, address, city, state, zip, phone_number, email)
             except Exception as e:
                 pass         
 
