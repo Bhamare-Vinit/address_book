14057facf531ad8e9c4f223891c5a1db007f9d0e
Bhamare-Vinit <vinitbhamare2002@gmail.com>
2024-07-11 14:22:48 +0530

    [vinit] add:updated code to addd multiple entry at a time

--- book.py
+++ book.py
@@ -3,6 +3,7 @@
 class Address_Book:
     def __init__(self):
         self.contact={}
+        
     def addContact(self,fname=None,lname=None,address=None,city=None,state=None,zip=None,phone_number=None,email=None):
         if fname!=None and lname!=None and address!=None and city!=None and state!=None and zip!=None and phone_number!=None and email!=None :
             if phone_number not in self.contact:
@@ -62,28 +63,30 @@ class Address_Book:
                 print("\n1. Add Contact\n2. Display Book\n3. Edit Contact\n4. Search Contact\n5. View Contacts\n6. Stop")
                 n = int(input("Enter your options: "))
                 if n == 1:
-                    fname = input("Enter first name: ")
-                    lname= input("Enter last name: ")
-                    address = input("Address: ")
-                    city=input("Enter the city: ")
-                    state=input("Enter the state: ")
-                    zip=input("Enter the zip code: ")
-                    phone_number = input("Phone Number: ")
-                    email = input("Email: ")
-                    if len(fname) == 0:
-                        fname = None
-                    if len(lname) == 0:
-                        lname = None
-                    if len(address) == 0:
-                        address = None
-                    if len(city) == 0:
-                        city = None
-                    if len(phone_number) == 0:
-                        phone_number = None
-                    if len(email) == 0:
-                        email = None
+                    n=int(input("Enter the number of entry you want to add: "))
+                    for i in range(n):
+                        fname = input("Enter first name: ")
+                        lname= input("Enter last name: ")
+                        address = input("Address: ")
+                        city=input("Enter the city: ")
+                        state=input("Enter the state: ")
+                        zip=input("Enter the zip code: ")
+                        phone_number = input("Phone Number: ")
+                        email = input("Email: ")
+                        if len(fname) == 0:
+                            fname = None
+                        if len(lname) == 0:
+                            lname = None
+                        if len(address) == 0:
+                            address = None
+                        if len(city) == 0:
+                            city = None
+                        if len(phone_number) == 0:
+                            phone_number = None
+                        if len(email) == 0:
+                            email = None
 
-                    self.addContact(fname, lname, address, city, state, zip, phone_number, email)
+                        self.addContact(fname, lname, address, city, state, zip, phone_number, email)
 
                 if n==2:
                     self.display()
